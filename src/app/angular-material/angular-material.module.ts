e046cafb26ee9fd14acaa37bf0f1ff56e92c1f48
adrianomisina <adriano.misina@gmail.com>
2024-03-25 15:10:58 -0300

    Criação do Layout do Componente de Filtro

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSelectModule} from '@angular/material/select';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   imports: [
     MatListModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule,
   ],
+
+  providers: [provideNativeDateAdapter()],
+
   exports: [
     MatListModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule
   ],
 })
 
