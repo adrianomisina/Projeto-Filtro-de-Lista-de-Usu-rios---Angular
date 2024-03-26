ec557c14b9dab0fea1e8489ff7d2de980d4c3964
adrianomisina <adriano.misina@gmail.com>
2024-03-26 15:30:29 -0300

    Criação do Pipe de Telefone

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { IUser } from '../../interfaces/user/user.interface';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrl: './user-details.component.scss'
+  styleUrl: './user-details.component.scss',
+
+  providers: [PipesModule]
 })
 export class UserDetailsComponent {
   @Input({required: true}) user: IUser = {} as IUser;
