efa6742f30964dab9c9dfb5a56304e29f2e59a37
adrianomisina <adriano.misina@gmail.com>
2024-03-28 10:40:48 -0300

    Armazenamento dos Valores dos Filtros

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
+import { IFilterOptions } from '../../interfaces/filter-0ptions-interface';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss'
 })
+
 export class FilterComponent {
-  foods = [
-    {
-      value: 'Teste',
-      viewValue: 'Teste'
-    }
-  ]
+  filterOptions: IFilterOptions = {
+    name: undefined,
+    startDate: undefined,
+    endDate: undefined,
+    status: undefined,
+  };
+
+  statusList = [
+    {description: 'Ativo', value: true},
+    {description: 'Inativo', value: false},
+  ];
+
+  onFilter() {
+    console.log(this.filterOptions)
+  };  
 }
